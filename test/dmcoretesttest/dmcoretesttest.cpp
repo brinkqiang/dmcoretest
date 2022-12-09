3b6ca1495931ad83e3219b0681073f0e21deb52b
brinkqiang <brink.qiang@gmail.com>
2022-12-09 19:47:16 +0800

    add

--- test/dmcoretesttest/dmcoretesttest.cpp
+++ test/dmcoretesttest/dmcoretesttest.cpp
@@ -1,13 +1,29 @@
 
-#include "dmcoretest.h"
+#include <time.h>
+#include <stdexcept>
 
-int main( int argc, char* argv[] ) {
 
-    Idmcoretest* module = dmcoretestGetModule();
-    if (module)
-    {
-        module->Test();
-        module->Release();
-    }
-    return 0;
+static void CrashFunction_INNER()
+{
+    int *i = reinterpret_cast<int*>(0x45);
+    *i = 5;  // crash!
+}
+
+static void CrashFunction()
+{   
+    srand(time(0));
+    int x = rand() % 2;
+    int y = 10;
+    int z = y / x;// crash!
+
+    printf("%d", z);
+    CrashFunction_INNER();
+}
+
+int main(int argc, char **argv) 
+{      
+    CrashFunction_INNER();
+
+    printf("did not crash?\n");   
+    return 0;   
 }
